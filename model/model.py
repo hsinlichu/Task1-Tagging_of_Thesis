9a998e2074b7c7e02f3c4c7863a777c59c7d3cd8
james60708 <b05505004@ntu.edu.tw>
2019-10-07 23:55:40 +0800

    add rnn model

--- model/model.py
+++ model/model.py
@@ -20,3 +20,35 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+
+class ThesisTaggingModel(BaseModel):
+    def __init__(self, dim_embeddings, num_classes, hidden_size=128,
+            num_layers=1,  rnn_dropout=0.2, clf_dropout=0.3, bidirectional=False):
+        super(rnn_net, self).__init__()
+        self.hidden_size = hidden_size
+        self.dim_embeddings = dim_embeddings
+        self.num_layers = num_layers
+        self.num_classes = num_classes
+        self.bidirectional = bool(bidirectional)
+        self.clf_dropout = clf_dropout
+
+        self.rnn = nn.LSTM(input_size=self.dim_embeddings, hidden_size=self.hidden_size,
+                num_layers=self.num_layers, bidirectional=self.bidirectional, batch_first=True) # , dropout=rnn_dropout
+        self.clf = nn.Sequential(
+                nn.Linear(hidden_size, hidden_size // 2),
+                nn.BatchNorm1d(hidden_size // 2),
+                nn.ReLU(),
+                nn.Dropout(self.clf_dropout),
+                nn.Linear(hidden_size // 2, num_classes)
+                )
+
+    def forward(self, sentence):
+        # sentence: torch.Size([64, 80, 300])
+        sentence_out, hidden = self.rnn(sentence)
+
+        last_output = sentence_out[:,-1,:]
+
+        score = self.clf(last_output)
+        #print(score.size())) # torch.Size([64, 37])
+        return score
